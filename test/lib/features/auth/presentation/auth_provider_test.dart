a0f2ad37e1e08dc39f407f152ec556eb20cc5387
Achudh Krishna A G <achudhk@gmail.com>
2024-03-22 12:22:22 +0530

    Add and fix test cases

--- test/lib/features/auth/presentation/auth_provider_test.dart
+++ test/lib/features/auth/presentation/auth_provider_test.dart
@@ -1,62 +1,199 @@
-import 'package:cg_flutter_implementation/features/auth/domain/repository/auth_repository.dart';
 import 'package:cg_flutter_implementation/features/auth/presentation/providers/auth_provider.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  testWidgets('Test Login State Provider', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      ProviderScope(
-        overrides: [
-          loginRepositoryProvider.overrideWithValue(FakeLoginRepository()),
-        ],
-        child: MaterialApp(
-          home: Consumer(builder: (context, ref, child) {
-            final state = ref.watch(loginStateProvider);
-            return Text(
-                'Username: ${state.username}, Password: ${state.password}');
-          }),
-        ),
-      ),
-    );
+  test('Login state provider initializes with default values', () {
+    // Create a ProviderContainer for testing
+    final container = ProviderContainer();
 
-    // Verify the initial state
-    expect(find.text('Username: , Password: '), findsOneWidget);
+    // Provide the necessary providers
+    container.read(loginStateProvider); // Read to trigger initialization
+    var loginState = container.read(loginStateProvider);
+
+    // Access the login state
+    loginState = container.read(loginStateProvider);
+
+    // Assert initial state values
+    expect(loginState.username, '');
+    expect(loginState.password, '');
+    expect(loginState.isLoading, false);
+    expect(loginState.error, null);
+    expect(loginState.obscurePassword, true);
   });
 
-  testWidgets('Test Login UseCase Provider', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      ProviderScope(
-        overrides: [
-          loginRepositoryProvider.overrideWithValue(FakeLoginRepository()),
-        ],
-        child: MaterialApp(
-          home: Consumer(builder: (context, ref, child) {
-            final useCase = ref.watch(loginUseCaseProvider);
-            return Text('Login UseCase: $useCase');
-          }),
-        ),
-      ),
-    );
+  test('Login state provider updates username', () {
+    final container = ProviderContainer();
+    container.read(loginStateProvider);
+    var loginState = container.read(loginStateProvider);
 
-    // Verify the use case is not null
-    expect(find.text('Login UseCase: '), findsOneWidget);
+    // Update username
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            username: 'test_user',
+          ),
+        );
+
+    // Assert updated username
+    expect(loginState.username, 'test_user');
+  });
+  test('Login state provider updates password', () {
+    final container = ProviderContainer();
+    container.read(loginStateProvider);
+    var loginState = container.read(loginStateProvider);
+
+    // Update password
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            password: 'secure_password',
+          ),
+        );
+
+    // Assert updated password
+    expect(loginState.password, 'secure_password');
+  });
+  test('Login state provider sets loading to true', () {
+    final container = ProviderContainer();
+    container.read(loginStateProvider);
+    var loginState = container.read(loginStateProvider);
+
+    // Set loading to true
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            isLoading: true,
+          ),
+        );
+
+    // Assert loading state
+    expect(loginState.isLoading, true);
   });
 
-  testWidgets('Test Login Repository Provider', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      ProviderScope(
-        child: MaterialApp(
-          home: Consumer(builder: (context, ref, child) {
-            final repository = ref.watch(loginRepositoryProvider);
-            return Text('Login Repository: $repository');
-          }),
-        ),
-      ),
-    );
+  test('Login state provider sets loading to false', () {
+    final container = ProviderContainer();
+    container.read(loginStateProvider);
+    var loginState = container.read(loginStateProvider);
 
-    // Verify the repository is not null
-    expect(find.text('Login Repository: '), findsOneWidget);
+    // Set loading to true (initially false)
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            isLoading: true,
+          ),
+        );
+
+    // Set loading to false
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            isLoading: false,
+          ),
+        );
+    // Assert loading state
+    expect(loginState.isLoading, false);
+  });
+  test('Login state provider sets error', () {
+    final container = ProviderContainer();
+    container.read(loginStateProvider);
+    var loginState = container.read(loginStateProvider);
+
+    // Set error message
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            error: 'Login failed',
+          ),
+        );
+
+    // Assert error state
+    expect(loginState.error, 'Login failed');
+  });
+
+  test('Login state provider clears error', () {
+    final container = ProviderContainer();
+    container.read(loginStateProvider);
+    var loginState = container.read(loginStateProvider);
+
+    // Set error message
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            error: 'Login failed',
+          ),
+        );
+
+    // Clear error
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            error: null,
+          ),
+        );
+
+    // Assert error state is null
+    expect(loginState.error, null);
+  });
+
+  test('Login state provider toggles password visibility', () {
+    final container = ProviderContainer();
+    container.read(loginStateProvider);
+    var loginState = container.read(loginStateProvider);
+
+    // Assert initial visibility
+    expect(loginState.obscurePassword, true);
+
+    // Toggle visibility
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            obscurePassword: false,
+          ),
+        );
+
+    // Assert visibility toggled
+    expect(loginState.obscurePassword, false);
+
+    // Toggle again
+    loginState = container
+        .read(
+          loginStateProvider.notifier,
+        )
+        .update(
+          (state) => state.copyWith(
+            obscurePassword: true,
+          ),
+        );
+
+    // Assert visibility back to true
+    expect(loginState.obscurePassword, true);
   });
 }
--- test/lib/features/home/data/repository/contacts_repository_test.dart
+++ test/lib/features/home/data/repository/contacts_repository_test.dart
@@ -1,74 +1,3 @@
-// import 'dart:convert';
-
-// import 'package:cg_flutter_implementation/features/home/data/entities/contacts.dart';
-// import 'package:cg_flutter_implementation/features/home/data/repository/contacts_repository.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:mockito/annotations.dart';
-// import 'package:mockito/mockito.dart';
-
-// import 'contacts_repository_test.mocks.dart';
-
-// @GenerateMocks([http.Client])
-// void main() {
-//   group('ContactsRepository', () {
-//     late ContactsRepository contactsRepository;
-//     late MockClient mockHttpClient;
-//     const baseUrl =
-//         'https://vtahoxgswvfwpdxpkoyn.hasura.ap-south-1.nhost.run/api/rest/cg-flutter-contact';
-//     final responseBody = {
-//       'cg_flutter_contacts': [
-//         {'id': '1', 'name': 'John Doe', 'email': 'john@example.com'},
-//         {'id': '2', 'name': 'Jane Smith', 'email': 'jane@example.com'}
-//       ]
-//     };
-
-//     setUp(() async {
-//       mockHttpClient = MockClient();
-//       when(mockHttpClient.get(Uri.parse(baseUrl))).thenAnswer(
-//           (_) async => http.Response(jsonEncode(responseBody), 200));
-//       contactsRepository = ContactsRepository(mockHttpClient);
-//     });
-
-//     test('getContacts - success', () async {
-//       // Arrange - Removed unnecessary comments
-
-//       // Act
-//       final contacts = await contactsRepository.getContacts();
-
-//       // Assert
-//       expect(contacts, isA<List<Contact>>()); // Ensure it's a list of Contact
-//       expect(contacts.length, 2);
-//       expect(contacts[0].id, '1');
-//       expect(contacts[0].name, 'John Doe');
-//       expect(contacts[0].email, 'john@example.com');
-//       expect(contacts[1].id, '2');
-//       expect(contacts[1].name, 'Jane Smith');
-//       expect(contacts[1].email, 'jane@example.com');
-//     });
-
-//     test('getContacts - failure (404 Not Found)', () async {
-//       // Arrange
-//       when(mockHttpClient.get(Uri.parse(baseUrl)))
-//           .thenAnswer((_) async => http.Response('Not Found', 404));
-
-//       // Act & Assert
-//       expect(() async => await contactsRepository.getContacts(),
-//           throwsA(isA<Exception>())); // Improved assertion
-//     });
-
-//     test('getContacts - failure (non-200 status code)', () async {
-//       // Arrange
-//       when(mockHttpClient.get(Uri.parse(baseUrl)))
-//           .thenAnswer((_) async => http.Response('Internal Server Error', 500));
-
-//       // Act & Assert
-//       expect(() async => await contactsRepository.getContacts(),
-//           throwsA(isA<Exception>())); // Improved assertion
-//     });
-//   });
-// }
-
 import 'dart:convert';
 
 import 'package:cg_flutter_implementation/features/home/data/entities/contacts.dart';
@@ -85,8 +14,7 @@ import 'contacts_repository_test.mocks.dart';
 void main() {
   late ContactsRepository contactsRepository;
   late MockClient mockHttpClient; // Use the generated MockClient
-  const baseUrl =
-      'https://example.com';
+  const baseUrl = 'https://example.com';
   final responseBody = {
     'cg_flutter_contacts': [
       {'id': '1', 'name': 'John Doe', 'email': 'john@example.com'},
@@ -121,14 +49,22 @@ void main() {
   });
 
   test('getContacts - failure (404 Not Found)', () {
-    when(mockHttpClient.get(Uri.parse(baseUrl)))
-        .thenAnswer((_) async => http.Response('Not Found', 404));
+    when(
+      mockHttpClient.get(
+        Uri.parse(baseUrl),
+        headers: {'content-type': 'application/json'},
+      ),
+    ).thenAnswer((_) async => http.Response('Not Found', 404));
     expect(contactsRepository.getContacts(), throwsException);
   });
 
   test('getContacts - failure (non-200 status code)', () {
-    when(mockHttpClient.get(Uri.parse(baseUrl)))
-        .thenAnswer((_) async => http.Response('Internal Server Error', 500));
+    when(
+      mockHttpClient.get(
+        Uri.parse(baseUrl),
+        headers: {'content-type': 'application/json'},
+      ),
+    ).thenAnswer((_) async => http.Response('Internal Server Error', 500));
     expect(contactsRepository.getContacts(), throwsException);
   });
 }
